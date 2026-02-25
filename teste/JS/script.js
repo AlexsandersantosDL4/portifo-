d6219de1312527fc9b4f5af9a578db9074ad58d4
AlexDL4 <alexsanderdossantosfarias@gmail.com>
2026-02-24 21:23:19 -0300

    mudei
    mudei

---
+++ teste/JS/script.js
@@ -0,0 +1,5 @@
+console.log(document.title);
+console.log(document.body);
+document.title = "Novo Título da Aba";
+document.URL = "C:\Users\VIC0268669\Desktop\PORT\portifo-";
+document.body.style.background = "yellow";
